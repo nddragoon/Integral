aa21177f8b53fa7f4c58044f7dc3b0d01384be65
nddragoon <nddragoon@gmail.com>
2023-02-05 17:25:30 -0600

    group some stuff

--- kubejs/client_scripts/REI.js
+++ kubejs/client_scripts/REI.js
@@ -9,3 +9,23 @@ onEvent("rei.hide.items", event => {
         event.hide(item);
     });
 })
+
+onEvent("rei.group", event => {
+    /* Groups together similar items with different NBT.
+    Stolen from KubeJS wiki; not entirely sure how it works,
+    but i think it makes an item object and then grabs the
+    namespace and id to feed to the function.
+    Not used for regular potions because any mod can add their
+    own and i dont want them to be in different groups.
+    TODO find a way to combine groups */
+    const useNbt = [
+        "apotheosis:potion_charm",
+        "immersiveengineering:potion_bucket",
+        "tconstruct:potion_bucket"
+    ];
+    useNbt.forEach(id => {
+        const item = Item.of(id)
+        const { namespace, path } = Utils.id(item.id)
+        event.groupSameItem(`kubejs:rei_groups/${namespace}/${path}`, item.name, item)
+    })
+})
